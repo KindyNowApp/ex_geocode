862351e14af540700c55ef436888a23b6febb9aa
Sina Karimi <creamycode@gmail.com>
2016-11-02 12:18:30 +1100

    Default api host

--- lib/config.ex
+++ lib/config.ex
@@ -2,7 +2,7 @@ defmodule ExGeocode.Config do
   @moduledoc """
   """
 
-  def api_host, do: Application.get_env(:ex_geocode, :api_host)
+  def api_host, do: Application.get_env(:ex_geocode, :api_host, "https://maps.googleapis.com")
 
   def base_url, do: api_host <> "/maps/api/geocode/json"
 
